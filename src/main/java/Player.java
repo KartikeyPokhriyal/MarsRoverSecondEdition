702a542275f805b494c7b59b31efa97d068c71fd
kpo <Pkartike@thoughtworks.com>
2018-06-01 21:47:57 +0530

    Added another marsRover Player

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -6,5 +6,9 @@
         MarsRover marsRover = new MarsRover(new Location(1,2,'N'));
 
         System.out.println(marsRover.follow("LMLMLMLMM"));
+
+        MarsRover marsRover2 = new MarsRover(new Location(3,3,'E'));
+
+        System.out.println(marsRover2.follow("MMRMMRMRRM"));
     }
 }
